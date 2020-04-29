ae89782fd6cd4ffa047f8eb81fc35deadefc7e94
Craig Gordon <cyghfer@gmail.com>
2020-04-29 19:08:56 -0400

    Add multiple .env file support

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: `./env/.env.${process.env.NODE_ENV || 'dev'}` });
 
 const Discord = require('discord.js');
 const { promisify } = require('util');
